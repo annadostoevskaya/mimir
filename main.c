109b9d5584f3d18cedbc280d95354d647b39a8d6
Anna Dostoevskaya <iwantknow.aboutjt68h43@gmail.com>
2026-06-12 02:25:36 +0500

    master :: arena allocator

--- main.c
+++ main.c
@@ -30,26 +30,41 @@ enum mimir_policy_engine {
 };
 
 struct mimir_policy {
-    mimir_policy_source_type source_type;
-    mimir_policy_repository_type repository_type;
-    mimir_policy_engine engine;
+    enum mimir_policy_source_type source_type;
+    enum mimir_policy_repository_type repository_type;
+    enum mimir_policy_engine engine;
     struct mimir_string source_path;
     struct mimir_string repository_path;
 };
 
 struct mimir_config_entry {
-    mimir_string section;
-    mimir_string key;
-    mimir_string value;
+    struct mimir_string section;
+    struct mimir_string key;
+    struct mimir_string value;
 };
 
-int mimir_loading_policy(const mimir_string content, struct mimir_policy *policy) {
+size_t mimir_config_parser_read_line(const struct mimir_string *content, struct mimir_string *line, size_t cursor) {
+    if (content->length - cursor > 0) { // TODO: Write assert
+        line->buffer = content->buffer + cursor;
+    }
+
+    line->buffer = content->buffer + cursor;
+    line->length = 1;
+
+    while (line->buffer[line->length] != '\n') {
+        line->length += 1;
+    }
+
+    return line->length - 1;
+}
+
+int mimir_loading_policy(const struct mimir_string *content, struct mimir_policy *policy) {
     mimir_string line = {};
     char c = '\0';
     mimir_string current_section = {};
     struct mimir_config_entry *mimir_config_entries = NULL; // TODO: Dynamic allocator (kinda vector)
 
-    while (mimir_config_parser_read_line(content, line)) {
+    while (mimir_config_parser_read_line(content, &line, )) {
         mimir_config_parser_trim_line(line);
 
         if (line.length == 0) { // line is empty
@@ -100,7 +115,7 @@ void mimir_error(const char* error_message) {
     mimir_write(STDERR_FILENO, "\n");
 }
 
-void *mimir_malloc(size_t size) {
+void *mimir_core_malloc(size_t size) {
     void *addr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
 
     if (addr == MAP_FAILED) {
@@ -111,7 +126,7 @@ void *mimir_malloc(size_t size) {
     return addr;
 }
 
-int mimir_free(void *addr, size_t size) {
+int mimir_core_free(void *addr, size_t size) {
     int success = 0;
 
     success = munmap(addr, size);
@@ -120,6 +135,71 @@ int mimir_free(void *addr, size_t size) {
         mimir_error("munmap() failed");
         return -1;
     }
+
+    return 0;
+}
+
+struct mimir_arena_allocator {
+    unsigned char *memory;
+    size_t size;
+    size_t cursor;
+};
+
+struct mimir_arena_allocator mimir_arena_initialize(size_t size) {
+    void *memory = mimir_core_malloc(size);
+
+    if (memory == NULL) {
+        mimir_error("Failed to initialize arena alloactor");
+        return (struct mimir_arena_allocator){ 0 };
+    }
+
+    return (struct mimir_arena_allocator){
+        .memory = memory,
+        .size = size,
+        .cursor = 0
+    };
+}
+
+void *mimir_arena_malloc(struct mimir_arena_allocator *arena, size_t size) {
+    void *memory;
+
+    if (arena == NULL) {
+        return NULL;
+    }
+
+    if (size > arena->size - arena->cursor) {
+        return NULL;
+    }
+
+    memory = (void*)(arena->memory + arena->cursor);
+    arena->cursor += size;
+
+    return memory;
+}
+
+void mimir_arena_mreset(struct mimir_arena_allocator *arena) {
+    if (arena != NULL) {
+        arena->cursor = 0;
+    }
+}
+
+int mimir_arena_destroy(struct mimir_arena_allocator *arena) {
+    if (arena == NULL) {
+        return -1;
+    }
+
+    if (arena->memory == NULL) {
+        return 0;
+    }
+
+    if (mimir_core_free(arena->memory, arena->size) != 0) {
+        mimir_error("Failed to destroy arena allocator, mimir_core_free() failed");
+        return -1;
+    }
+
+    arena->memory = NULL;
+    arena->size = 0;
+    arena->cursor = 0;
 
     return 0;
 }
@@ -147,7 +227,7 @@ int main(int argc, char **argv) {
 
     // TODO: Write custom alloactor here!
     char *policy_content = NULL;
-    policy_content = (char*)mimir_malloc(policy_stat.st_size);
+    policy_content = (char*)mimir_core_malloc(policy_stat.st_size);
 
     if (policy_content == NULL) {
         mimir_error("Failed to allocate memory for backup policy");
@@ -167,7 +247,7 @@ int main(int argc, char **argv) {
     }
 
     int success = 0;
-    success = mimir_free((void*)policy_content, policy_stat.st_size);
+    success = mimir_core_free((void*)policy_content, policy_stat.st_size);
     if (success != 0) {
         mimir_error("Failed to free memory for backup policy");
         return -1;
